cee4761dc97d25c89f8e32c22a0bbf788acdcc85
Thokozani Mntambo <thokozanimntamb@gmail.com>
2026-04-17 15:11:24 +0200

    connecting MongoDB

--- Server/Index.js
+++ Server/Index.js
@@ -1,18 +1,24 @@
-const express = require("express")
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const dotenv = require('dotenv')
 
-
-
-
+dotenv.config()
 
 const app = express()
-const PORT = process.env.PORT || 5000
 
+// Middleware
+app.use(cors())
+app.use(express.json())
 
-app.get("/" , (req , res)=>{
-res.send("ApplyIQ API is running....")
-
+// Test route
+app.get('/', (req, res) => {
+  res.send('ApplyIQ API is running...')
 })
 
-app.listen(PORT , ()=>{
-    console.log(`Server is running on port ${PORT}`)
+// Port
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
 })
